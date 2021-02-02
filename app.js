856a3e60d9357591864347c270d71c2109fd46fb
dev-monir <monir.grb@gmail.com>
2021-02-02 11:36:24 +0600

    finish js

--- app.js
+++ app.js
@@ -10,18 +10,29 @@ const loginArea = document.getElementById("login").addEventListener("click", fun
 
 const depositBtn = document.getElementById("add-deposit");
 depositBtn.addEventListener("click", function () {
-    const depositAmount = document.getElementById("depositAmount").value;
-    const depositNumber = parseFloat(depositAmount);
-
+    const depositNumber = getInputNumber("depositAmount");
+    updateSpanText("currentDeposit", depositNumber);
+    updateSpanText("currentBalance", depositNumber);
+    document.getElementById("depositAmount").value = "";   
+})
 function updateSpanText(id, depositNumber) {
     const current = document.getElementById(id).innerText;
     const currentNumber = parseFloat(current);
     const total = depositNumber + currentNumber;
     document.getElementById(id).innerText= total;
 }
-    updateSpanText("currentDeposit", depositNumber);
-    updateSpanText("currentBalance", depositNumber);
-    document.getElementById("depositAmount").value = ""; 
-    
+
+// withdraw event handler
+const withdrawBtn = document.getElementById("withdrawBtn");
+withdrawBtn.addEventListener("click", function () {
+    const withdrawNumber = getInputNumber("withdrawAmount");
+    updateSpanText("currentWithdraw", withdrawNumber); 
+    updateSpanText("currentBalance", -1*withdrawNumber);
+    document.getElementById("withdrawAmount").value = ""; 
 })
 
+function getInputNumber(id) {
+    const withdrawAmount = document.getElementById(id).value;
+    const withdrawAmountNumber = parseFloat(withdrawAmount);
+    return withdrawAmountNumber
+}
