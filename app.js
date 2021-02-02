927a1bbb012679bccf1271ab068e063a28960451
dev-monir <monir.grb@gmail.com>
2021-02-02 10:21:53 +0600

    Added deposit function

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
+// button login button handler
 const loginArea = document.getElementById("login").addEventListener("click", function () {
-    document.getElementById("login-area").style.display="none";
+    const loginArea = document.getElementById("login-area");
+    loginArea.style.display="none";
+    transactionArea = document.getElementById("transaction-area");
+    transactionArea.style.display="block";
 })
+
+// deposit event handler
+
+const depositBtn = document.getElementById("add-deposit");
+depositBtn.addEventListener("click", function () {
+    const depositAmount = document.getElementById("depositAmount").value;
+    const depositNumber = parseFloat(depositAmount);
+
+function updateSpanText(id, depositNumber) {
+    const current = document.getElementById(id).innerText;
+    const currentNumber = parseFloat(current);
+    const total = depositNumber + currentNumber;
+    document.getElementById(id).innerText= total;
+}
+    updateSpanText("currentDeposit", depositNumber);
+    updateSpanText("currentBalance", depositNumber);
+    document.getElementById("depositAmount").value = ""; 
+    
+})
+
